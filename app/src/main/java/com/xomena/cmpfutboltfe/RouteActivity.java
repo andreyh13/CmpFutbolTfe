e36286204030f2abc168537671cb131c77176624
Xomena <andreyh.13@gmail.com>
2017-04-29 00:00:06 +0200

    added curved dashed lines

--- app/src/main/java/com/xomena/cmpfutboltfe/RouteActivity.java
+++ app/src/main/java/com/xomena/cmpfutboltfe/RouteActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
@@ -26,16 +27,21 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.PolyUtil;
+import com.google.maps.android.SphericalUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -144,6 +150,7 @@ public class RouteActivity extends AppCompatActivity implements
                 @Override
                 public void onMapReady(GoogleMap googleMap) {
                     if(googleMap!=null) {
+                        LatLng lastLatLng = null;
                         try {
                             if (jsonRoute.has("status") && jsonRoute.getString("status").equals("OK")) {
                                 if (jsonRoute.has("routes")) {
@@ -188,8 +195,9 @@ public class RouteActivity extends AppCompatActivity implements
                                                     //End address
                                                     if(i==al.length()-1 && l.has("end_address") && !l.isNull("end_address")){
                                                         if (l.has("end_location") && !l.isNull("end_location")) {
+                                                            lastLatLng = new LatLng(l.getJSONObject("end_location").getDouble("lat"), l.getJSONObject("end_location").getDouble("lng"));
                                                             googleMap.addMarker(new MarkerOptions()
-                                                                    .position(new LatLng(l.getJSONObject("end_location").getDouble("lat"), l.getJSONObject("end_location").getDouble("lng")))
+                                                                    .position(lastLatLng)
                                                                     .title(l.getString("end_address"))
                                                                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
                                                         }
@@ -204,9 +212,14 @@ public class RouteActivity extends AppCompatActivity implements
                             Log.e(LOG_TAG, "Cannot process directions JSON results", e);
                         }
                         if(ff!=null) {
-                            googleMap.addMarker(new MarkerOptions().position(new LatLng(ff.getLat(), ff.getLng()))
+                            LatLng fldLatLng = new LatLng(ff.getLat(), ff.getLng());
+                            googleMap.addMarker(new MarkerOptions().position(fldLatLng)
                                     .title(ff.getName()).snippet(getString(R.string.phoneLabel) + " " + ff.getPhone())
                                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_soccerfield)));
+
+                            if (lastLatLng != null) {
+                                self.showCurvedPolyline(fldLatLng,lastLatLng,0.5);
+                            }
                         }
                         googleMap.setOnMyLocationButtonClickListener(self);
                         self.mMap = googleMap;
@@ -296,4 +309,40 @@ public class RouteActivity extends AppCompatActivity implements
             }
         }
     }
+
+    private void showCurvedPolyline (LatLng p1, LatLng p2, double k) {
+        //Calculate distance and heading between two points
+        double d = SphericalUtil.computeDistanceBetween(p1,p2);
+        double h = SphericalUtil.computeHeading(p1, p2);
+
+        //Midpoint position
+        LatLng p = SphericalUtil.computeOffset(p1, d*0.5, h);
+
+        //Apply some mathematics to calculate position of the circle center
+        double x = (1-k*k)*d*0.5/(2*k);
+        double r = (1+k*k)*d*0.5/(2*k);
+
+        LatLng c = SphericalUtil.computeOffset(p, x, h + 90.0);
+
+        //Polyline options
+        PolylineOptions options = new PolylineOptions();
+        List<PatternItem> pattern = Arrays.<PatternItem>asList(new Dash(30), new Gap(20));
+
+        //Calculate heading between circle center and two points
+        double h1 = SphericalUtil.computeHeading(c, p1);
+        double h2 = SphericalUtil.computeHeading(c, p2);
+
+        //Calculate positions of points on circle border and add them to polyline options
+        int numpoints = 100;
+        double step = (h2 -h1) / numpoints;
+
+        for (int i=0; i < numpoints; i++) {
+            LatLng pi = SphericalUtil.computeOffset(c, r, h1 + i * step);
+            options.add(pi);
+        }
+
+        //Draw polyline
+        mMap.addPolyline(options.width(10).color(Color.BLUE).geodesic(false).pattern(pattern));
+    }
+
 }
